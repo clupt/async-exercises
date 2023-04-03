a797a308460c6be7839ab30e6dc795d9a6fdd9f3
mgregerson <mgregerson4321@gmail.com>
2023-04-03 19:59:34 -0400

    completed step two - multiple nums

--- number_facts/app.js
+++ number_facts/app.js
@@ -1,24 +1,44 @@
 "use strict";
-const BASE_URL = 'http://numbersapi.com/';
+const BASE_URL = "http://numbersapi.com/";
 
-const $results = $('#lucky-results');
-
+const $results = $("#lucky-results");
 
 async function processForm(evt) {
   evt.preventDefault();
 
-  const num = $('#luckyNum').val()
-  const year = $('#year').val()
+  const num = $("#luckyNum").val();
 
-  const resp = await axios.get(`${BASE_URL}${num}?json`)
+  const resp = await axios.get(`${BASE_URL}${num}?json`);
   console.log("resp=", resp);
-  showResults(resp.data)
-};
-
-function showResults(num){
-  $results.html(`
-    <p>Your lucky num is <b>${num.number}</b> (${num.text})</p>
-  `)
+  showResults(resp.data);
 }
 
-$("#lucky-form").on("submit", processForm);
+function showResults(num) {
+  $results.html(`
+    <p>Your lucky num is <b>${num.number}</b> (${num.text})</p>
+  `);
+}
+
+async function processFormWithMultipleArgs(evt) {
+  evt.preventDefault();
+
+  const num = $("#luckyNum").val();
+  const num2 = $("#luckyNum2").val();
+  const num3 = $("#luckyNum3").val();
+
+  const resp = await axios.get(`${BASE_URL}${num},${num2},${num3}?json`);
+  console.log("resp=", resp);
+  showMultipleResults(resp.data);
+}
+
+function showMultipleResults(numbers) {
+  let values = Object.values(numbers);
+  console.log(Object.values(numbers));
+  for (let value of values) {
+    $results.append(`
+    <p><b>${value}</b></p>
+  `);
+  }
+}
+
+$("#lucky-form").on("submit", processFormWithMultipleArgs);
