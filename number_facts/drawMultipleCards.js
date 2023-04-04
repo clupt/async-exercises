db235db662c22246ec0192cd5be8f1e3414b52fa
Chris Lupton <42554017+clupt@users.noreply.github.com>
2023-04-03 20:12:09 -0500

    fix typo

--- number_facts/drawMultipleCards.js
+++ number_facts/drawMultipleCards.js
@@ -12,26 +12,16 @@ async function getDeck (evt){
 }
 
 async function drawCards(deck){
-  const cardPromiseOne = axios.get(
+  const resp = axios.get(
     `https://deckofcardsapi.com/api/deck/${deck}/draw/?count=1`
   )
 
-  const cardPromiseTwo = axios.get(
+  const resp2 = axios.get(
     `https://deckofcardsapi.com/api/deck/${deck}/draw/?count=1`
   )
   // console.log("value=", resp.data.cards[0].value)
   // console.log("suit=", resp.data.cards[0].suit)
-  const cards = await Promise.allSettled([
-    cardPromiseOne,
-    cardPromiseTwo
-  ])
-
-  console.log("cards=", cards);
-
-  for(let card of cards){
-    console.log("value=", card.value.data.cards[0].value)
-    console.log("suit=", card.value.data.cards[0].suit)
-  }
+  promises
 }
 
 
